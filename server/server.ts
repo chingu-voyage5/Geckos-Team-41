45660f94b4348d2f395cbf313e03ff8b274cbbd9
Stanley Thijssen <stanley.thijssen@energybox.com>
2018-06-04 23:50:12 +0800

    Setup basic TypeScript server file

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,34 @@
 import * as express from 'express';
-import { ExampleController } from './controllers/example.controller';
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+import * as morgan from 'morgan';
 
-const app: express.Application = express();
-const port: number = process.env.PORT || 3000;
+class Server {
+  public app: express.Application;
 
-app.use('/example', ExampleController);
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
 
-app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}/`);
-});
+  public config() {
+
+    const MONGO_URI = 'mongodb://localhost/test';
+    mongoose.connect(process.env.MONGODB_URI || MONGO_URI);
+
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+    this.app.use(morgan('dev'));
+
+  }
+
+  public routes(): void {
+    let router: express.Router;
+    router = express.Router();
+
+    this.app.use('/', router);
+  }
+}
+
+export default new Server().app;
